28d410772cacace51a4ad47ea13f9905521b8b29
Akshay Manchanda <akshaykmanchanda@gmail.com>
2020-02-04 09:51:18 -0500

    Added an example of styling

---
+++ client/src/components/styledComponents/NavButton.js
@@ -0,0 +1,20 @@
+import styled, { css } from "styled-components"
+
+const SignupButton = styled.button`
+  width: 500px;
+  font-size: 50px;
+  color: blue;
+`
+// ${props =>
+//   props.theme == "light" &&
+//   css`
+//     background: white;
+//     color: dark;
+//   `}
+// ${props =>
+//   props.theme == "dark" &&
+//   css`
+//     background: black;
+//     color: white;
+//   `}
+export default SignupButton
