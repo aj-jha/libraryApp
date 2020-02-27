d1ac5cf994c2052b7ea22187467bbed7680496e1
azhaorbi <azhao@timhortons.com>
2020-02-27 15:36:14 -0500

    login and signup tests

--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -6,9 +6,9 @@ const HomeView = () => {
       <h1>Welcome to Katherine's Library</h1>
       <p>
         Here lies the collection of books that define Katherine McTaggert's life
-        as an instagram influencer and full time DILF Hunter
+        as an instagram influencer.
       </p>
-      <p>Please be respectful of Katherine's life's work</p>
+      <p>Please be respectful of Katherine's life's work.</p>
     </div>
   )
 }
